6c2863249f84603990ae9f3d97b4d93fccc3fb24
nawaz quazi <nawazquazi356@gmail,com>
2023-02-09 00:04:02 +0530

    My sqrt1

---
+++ Question_12.java
@@ -0,0 +1,27 @@
+public class Question_12 {
+    public static int mySqrt(int x) {
+        int i = (int) Math.sqrt(x);
+        return i;
+    }
+
+    public static int mySqrt1(int x) {
+         int ans = 0;
+         for(int i =1; i<=x;i++){
+           if(i*i>x){
+           ans=i-1;
+           break;
+           }
+           if(i*i==x){
+           ans=i;
+           break;
+           }
+         }
+         return ans;
+     }
+
+    public static void main(String[] args) {
+        System.out.println(mySqrt1(4));
+        System.out.println(mySqrt1(8));
+
+    }
+}
