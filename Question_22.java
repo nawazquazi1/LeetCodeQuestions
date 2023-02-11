d9eb10c38c176b04336c27fa4727e0833d9696b2
nawaz quazi <nawazquazi356@gmail,com>
2023-02-11 23:24:27 +0530

    Power of two

---
+++ Question_22.java
@@ -0,0 +1,19 @@
+public class Question_22 {
+
+    public static boolean isPowerOfTwo(int n) {
+        if(n<=0) {
+            return false;
+        }
+        while(n%2==0){
+            n/=2;
+        }
+        return n==1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(1));
+        System.out.println(isPowerOfTwo(16));
+        System.out.println(isPowerOfTwo(3));
+
+    }
+}
