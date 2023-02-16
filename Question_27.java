fd554f7e543ad3db081c01800ce15e8cb69df51d
nawaz quazi <nawazquazi356@gmail,com>
2023-02-16 21:07:46 +0530

    Rotate array

---
+++ Question_27.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Question_27 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n1 = scn.nextInt();
+        int[] a = new int[n1];
+        for (int i = 0; i < n1; i++) {
+            a[i] = scn.nextInt();
+        }
+        int ro = scn.nextInt();
+        rotate(a, ro);
+        display(a);
+    }
+
+    public static void display(int[] a) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int val : a) {
+            sb.append(val).append(" ");
+        }
+        System.out.println(sb);
+    }
+
+    public static void reverse(int[] a, int li, int ri) {
+        while (li < ri) {
+            int temp = a[li];
+            a[li] = a[ri];
+            a[ri] = temp;
+
+            li++;
+            ri--;
+        }
+    }
+
+    public static void rotate(int[] a, int k) {
+        k = k % a.length;
+        if (k < 0) {
+            k += a.length;
+        }
+
+        reverse(a, 0, a.length - k - 1);
+        reverse(a, a.length - k, a.length - 1);
+        reverse(a, 0, a.length - 1);
+    }
+
+}
