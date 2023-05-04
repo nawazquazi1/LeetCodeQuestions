ff9e58869f6f9c3104a784496d5e7045cf550f4a
nawaz quazi <nawazquazi356@gmail.com>
2023-05-05 02:13:55 +0530

    New Question

--- Question_44.java
+++ Question_44.java
@@ -1,5 +1,15 @@
+import java.io.FileInputStream;
+import java.util.HashSet;
+
 public class Question_44 {
     public boolean isMatch(String s, String p) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 4, 5, 1, 6};
+        int n = 0;
+
 
     }
 }
